cad5316a6f1bbf1714a22c1f242d63c4d6292d82
Dan Miller <x.dan.miller@outlook.com>
2014-06-19 12:43:54 -0400

    Second failing test

--- FizzBuzzTests.py
+++ FizzBuzzTests.py
@@ -7,5 +7,11 @@ class FizzBuzzTests(unittest.TestCase):
         expected = '1'
         self.assertEqual(result, expected)
 
+    def test_get_answer_2(self):
+        result = FizzBuzz.get_answer(2)
+        expected = '2'
+        self.assertEqual(result, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
