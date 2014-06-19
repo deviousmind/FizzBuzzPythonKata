fd4c2fc1a3a17f3b6c8733b3ca51a793e2771291
Dan Miller <x.dan.miller@outlook.com>
2014-06-19 12:47:50 -0400

    Simplified logic

--- FizzBuzzModule.py
+++ FizzBuzzModule.py
@@ -2,10 +2,5 @@ class FizzBuzz:
 
     @staticmethod
     def get_answer(number):
-        if(number == 1):
-            return '1'
-        elif(number == 2):
-            return '2'
-
-        return ''
+        return number.__str__()
 
