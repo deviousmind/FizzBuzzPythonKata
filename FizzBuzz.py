9c65f494a925bfcf7eb166d5eb90a8da118d655c
Dan Miller <x.dan.miller@outlook.com>
2014-08-03 15:59:37 -0400

    REALLY added run script.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,8 @@
-__author__ = 'dj'
+import FizzBuzzModule
+
+
+if __name__ == '__main__':
+    number = 0
+    for number in range(101):
+        result = FizzBuzzModule.get_answer(number)
+        print(result)
